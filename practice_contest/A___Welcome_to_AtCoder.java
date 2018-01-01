73ef93352d85199737e1e7075761ca3dcfc4cf6f
Tom <Tom@Tom-no-MacBook-Pro.local>
2018-01-01 16:36:13 +0900

    submission

--- practice_contest/A___Welcome_to_AtCoder.java
+++ practice_contest/A___Welcome_to_AtCoder.java
@@ -1,5 +1,28 @@
 package practice_contest;
 
-public class A___Welcome_to_AtCoder {
+import java.util.*;
 
+class A___Welcome_to_AtCoder {
+
+	public static void main(String[] args) {
+
+		try {
+
+			Scanner sc = new Scanner(System.in);
+			
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+
+			String s = sc.next();
+			
+			System.out.println((a+b+c) + " " + s);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class Main {
 }
