cbd9978e064241cd8e9b26e6f23b0e5b64c7e0fc
Tom <Tom@Tom-no-MacBook-Pro.local>
2018-01-05 14:34:59 +0900

    adjustment

---
+++ practice_contest/A.java
@@ -0,0 +1,28 @@
+// Welcome to AtCoder
+
+
+package practice_contest;
+
+import java.util.*;
+
+class Main {
+
+	public static void main(String[] args) {
+
+		try {
+
+			Scanner sc = new Scanner(System.in);
+			
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+
+			String s = sc.next();
+			
+			System.out.println((a+b+c) + " " + s);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
